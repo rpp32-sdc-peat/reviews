324e9485cefe33a46d31a483e6e67dbd6bb8057c
Peter Park <mac-main@Peters-MBP-2.lan>
2022-01-26 14:48:14 -0800

    Routed Product Overview Router

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const expressStaticGzip = require("express-static-gzip");
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = 3000;
@@ -9,7 +11,9 @@ const productOverviewRouter = require('./routes/productOverview.js');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/../client'));
+app.use("/", expressStaticGzip(path.join(__dirname, 'client', 'dist'), {
+  enableBrotli: true
+}));
 
 // Place your routers
 app.use('/productOverview', productOverviewRouter);
